cffe49d1ee63cfa6d341d7e951f8d1374e9aeb88
viviags28 <viviags28@gmail.com>
2026-05-21 10:40:59 +0700

    Update unit_test.go

--- report-service/unit_test.go
+++ report-service/unit_test.go
@@ -2,16 +2,23 @@ package main
 
 import (
 	"context"
+	"database/sql"
 	"testing"
 )
 
 func TestGetDailyReport_ShouldReturnReport(t *testing.T) {
-	svc := NewReportService()
-	report, err := svc.GetDailyReport(context.Background(), "2026-04-25")
+
+	svc := NewReportService(&sql.DB{})
+
+	report, err := svc.GetDailyReport(
+		context.Background(),
+		"2026-04-25",
+	)
 
 	if report == nil {
 		t.Errorf("Expected report not nil, got nil")
 	}
+
 	if err != nil {
 		t.Errorf("Expected no error, got %v", err)
 	}
