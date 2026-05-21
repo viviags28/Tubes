672bf42ad96755adba7ad6a0ed0c6842bda6f270
viviags28 <viviags28@gmail.com>
2026-05-21 10:40:40 +0700

    Update main.go

--- report-service/main.go
+++ report-service/main.go
@@ -6,11 +6,15 @@ import (
 )
 
 func main() {
-	svc := NewReportService()
+
+	ConnectDB()
+
+	svc := NewReportService(DB)
 	h := NewReportHandler(svc)
 
 	http.HandleFunc("/report/daily", h.DailyReport)
 
 	log.Println("Report service running on :8083")
+
 	log.Fatal(http.ListenAndServe(":8083", nil))
 }
