de06b115d51b042be253034914dfdfe55201f15d
viviags28 <viviags28@gmail.com>
2026-05-21 10:41:11 +0700

    Delete model.go

--- report-service/report-service/model.go
+++
@@ -1,9 +0,0 @@
-package main
-
-type DailyReport struct {
-	TotalPaket  int     `json:"total_paket"`
-	Delivered   int     `json:"delivered"`
-	Pending     int     `json:"pending"`
-	Terlambat   int     `json:"terlambat"`
-	RataRataETA float64 `json:"rata_rata_eta"`
-}
